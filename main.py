020e85f9e6bff6ee73f51a773811fc1eab55d411
explosion33 <38818379+explosion33@users.noreply.github.com>
2018-11-27 21:44:54 -0800

    Update main.py
    fixed high value tile colors and font sizes, fixed bug where tiles when moving down or right would merge incorectly, added win detection and screen(Continue and reset options)
    
    -General linting fixes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pygame
 import extras
 
 
-class tileObj(object):
+class tileObj:
     """
     num: Tile Number
 
@@ -47,9 +47,9 @@ class tileObj(object):
         "The Code to generate a tile"
         tile = pygame.Surface((132, 132))
         return tile
-    def addNum(self, tile):
+    def addNum(self, tile, size):
         "The code to add the number to the blank tile"
-        font = pygame.font.SysFont('', 145)
+        font = pygame.font.SysFont('', size)
         if self.value != 2 and self.value != 4:
             text = font.render(str(self.value), True, (255, 255, 255))
         else: text = font.render(str(self.value), True, (0, 0, 0))
@@ -61,6 +61,7 @@ class tileObj(object):
     def pickColor(self):
         "Defines preset colors for values"
         v = self.value
+        size = 145
 
         if v == 2: c = (230, 230, 230)
         elif v == 4: c = (203, 203, 203)
@@ -68,20 +69,31 @@ class tileObj(object):
         elif v == 16: c = (255, 170, 0)
         elif v == 32: c = (255, 112, 77)
         elif v == 64: c = (179, 36, 0)
-        elif v == 128: c = (255, 255, 153)
-        elif v == 256: c = (255, 255, 51)
-        elif v == 512: c = (230, 230, 0)
-        elif v == 1024: c = (179, 179, 0)
+        elif v == 128:
+            c = (255, 255, 153)
+            size = 100
+        elif v == 256:
+            c = (255, 255, 51)
+            size = 90
+        elif v == 512:
+            c = (230, 230, 0)
+            size = 90
+        elif v == 1024:
+            c = (255,223,0)
+            size = 80
         else:
-            c = (153, 153, 0)
+            c = (255,223,0)
+            size = 80
+        if v > 10000: size = 60
 
-        return c
+        return c,size
 
     def draw(self):
         "Draws the tile onto the screen surface at a specific coordinate point"
         tile = self.genTile()
-        tile.fill(self.pickColor())
-        tile = self.addNum(tile)
+        c,size = self.pickColor()
+        tile.fill(c)
+        tile = self.addNum(tile,size)
 
         offset = 165
         x = 146.25*self.pos[0]
@@ -111,13 +123,14 @@ def spawnTile(grid):
 
 def sortTiles(tiles, move):
     "Sorts tiles in a list based on keyPress"
+
     if move[0] != 0:
         t = []
         while tiles:
             close = tiles[0]
             for tile in tiles:
                 p = tile.pos[0]
-                if abs(move[0]-p) < abs(move[0]-close.pos[0]):
+                if abs((move[0]*5)-p) < abs((move[0]*5)-close.pos[0]):
                     close = tile
             t.append(close)
             tiles.remove(close)
@@ -128,11 +141,12 @@ def sortTiles(tiles, move):
             close = tiles[0]
             for tile in tiles:
                 p = tile.pos[1]
-                if abs(move[1]-p) < abs(move[1]-close.pos[1]):
+                if abs((move[1]*5)-p) < abs((move[1]*5)-close.pos[1]):
                     close = tile
             t.append(close)
             tiles.remove(close)
         return t
+    return t
 
 def reset():
     "Resets game variables for purpous of the retry button"
@@ -160,6 +174,21 @@ def reset():
     fadeIn = [True,0]
     score = [0,0]
 
+def cont():
+    "Function to be used for the continue button on winning"
+    global movement
+    global keySpam
+    global mode
+    global fadeIn
+    global score
+    global canWin
+
+    movement = (0,0)
+    keySpam = False
+    mode = 'game'
+    fadeIn = [True,0]
+    score = [0,0]
+    canWin = False
 
 def onKeyPress(velocity, grid, tiles):
     "Logic for when a key is pressed"
@@ -204,10 +233,6 @@ def onKeyPress(velocity, grid, tiles):
 
     ##--##
 
-
-
-
-
     if movedOnce:
         newTile,grid = spawnTile(grid)
         t.append(newTile)
@@ -239,6 +264,10 @@ def onKeyPress(velocity, grid, tiles):
             global mode
             mode = 'lose'
 
+    for tile in t:
+        global canWin
+        if tile.value == 2048 and canWin:
+            mode = 'win'
 
     return grid, t
 
@@ -269,7 +298,7 @@ keySpam = False
 mode = 'game'
 fadeIn = [True, 0]
 score = [0,0]
-
+canWin = True
 
 
 print(grid)
@@ -328,7 +357,6 @@ while True:
         elif not keys:
             keySpam = True
         ##--##
-
 
     if mode == 'lose':
         pygame.draw.rect(screen,(125,125,125),border)
@@ -394,6 +422,78 @@ while True:
 
         if not fadeIn[0]:
             resetB.render(screen)
+            screen.blit(scoreText, (size[0]/2 - scoreText.get_width()/2, 210))
+            screen.blit(title, (size[0]/2 - title.get_width()/2 ,60))
+
+            score[0] = score[1]
+
+    if mode == 'win':
+        pygame.draw.rect(screen,(125,125,125),border)
+        border_width = 15
+        pygame.draw.rect(screen,(155,155,155),pygame.Rect(border.left+border_width,border.top+border_width,border.width-(border_width*2),border.height-(border_width*2)))
+
+        for i in range(3):
+            start = border.left + border_width/2
+            step = border.width/4 - border_width/4
+            start = start + (step*(1+i))
+            start = int(start)
+            end = (start,border.bottom-5)
+            start = (start,border.top+5)
+
+            pygame.draw.line(screen,(125,125,125),start,end,border_width)
+
+            s,d = start
+            x,c = end
+            start = (d,s)
+            end = (c,x)
+            pygame.draw.line(screen,(125,125,125),start,end,border_width)
+
+        ##DrawTiles
+        for tile in tiles:
+            tile.draw()
+
+        if fadeIn[0]:
+            score[1] = 0
+            for tile in tiles:
+                score[1] += tile.value
+
+            score[0] += score[1]*(5/220)
+
+            chng = 5
+            fadeIn[1] = fadeIn[1] + 5
+
+            if fadeIn[1] == 220:
+                fadeIn[0] = False
+
+        gold = pygame.Surface((size[0],size[1]))
+        gold.fill((255,215,0))
+
+        font = pygame.font.SysFont('', 155)
+
+        title = font.render('2048', True, (255,255,255))
+        gold.blit(title, (size[0]/2 - title.get_width()/2 ,60))
+
+        font = pygame.font.SysFont('', 135)
+
+        scoreText = font.render('Score: ' + str(int(score[0])), True, (255,255,255))
+        gold.blit(scoreText, (size[0]/2 - scoreText.get_width()/2, 210))
+
+        contB = extras.word('Continue',['center',360],cont,[],'',(255,255,255),(255,215,120),135)
+        resetB = extras.word('Play Again',['center',490],reset,[],'',(255,255,255),(200,200,200),135)
+
+        if fadeIn[0]:
+            resetB.render(gold)
+            contB.render(gold)
+
+        resetB.isHovered(mouse)
+        contB.isHovered(mouse)
+
+        gold.set_alpha(fadeIn[1])
+        screen.blit(gold, (0,0))
+
+        if not fadeIn[0]:
+            resetB.render(screen)
+            contB.render(screen)
             screen.blit(scoreText, (size[0]/2 - scoreText.get_width()/2, 210))
             screen.blit(title, (size[0]/2 - title.get_width()/2 ,60))
 
